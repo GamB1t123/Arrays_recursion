08be316ae5f438ffed1e396df3d391c1097cb07d
GamB1t123 <stoianovskiy@gmail.com>
2021-11-26 17:01:50 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,88 +1,288 @@
 <?php
-//1.Рекурсивно выведите на экран ряд Фибоначчи до 10-го элемента
-function Fib($number){
-    // if and else if to generate first two numbers
-    if ($number == 0)
-        return 0;
-    else if ($number == 1)
-        return 1;
-    // Recursive Call to get the upcoming numbers
-    else
-        return (Fib($number-1) + Fib($number-2));
+
+
+interface Building
+{
+
 }
 
-$number = 10;
-for ($i = 0; $i < $number; $i++){
-    echo Fib($i),' ';
-}
-//2.Создайте в цикле (for либо while) одномерный массив из 10 элементов,
-// где значение на каждой итерации - произвольное число от 1 до 10 (используйте функцию mt_rand)
+class House implements Building
+{
+    public $numBuilding;
+    public $numApartments;
+    public $numFloors;
 
-$arr = [];
-for ($j = 0; $j <= 10; $j++) {
-    $arr[] = mt_rand(1, 10);
-}
-echo '<pre>';
-print_r($arr);
-echo '</pre>';
-//3.Выведите все чётные и все нечётные элементы массива из задания 2
-foreach ($arr as $key => $value) {
-    if ($value % 2 != 0) {
-        echo "$value <br>";
-    }
-}
-    foreach ($arr as $k =>$v) {
-        if ($v % 2 == 0) {
-            echo  "$v <br>";
-        }
-    }
-//4.Отсортируйте массив из задания 2 по ключу в обратном порядке
-sort($arr);
-    foreach ($arr as $key => $value)
+
+    public function getNumApartments()
     {
-      echo  "$key = $value\n";
+        return $this->numApartments;
     }
 
-
-//5.Отсортируйте массив из задания 3 по значению в обратном порядке
-echo '<br>';
-rsort($arr);
-    foreach ( $arr as $key => $value)
+    public function setNumApartments($numApartments)
     {
-        echo "$key = $value\n";
+        $this->numApartments = $numApartments;
     }
 
-//6.Создайте индексированный массив произвольных имён (до 10 элементов).
-// Поменяйте местами ключи и значения этого массива и выведите на экран
-
-$someArr = ['Bob','Alfred','Lily','Maks','Bill','Mark'];
-echo '<br>';
-    $swap = array_flip($someArr);
-    var_dump($swap);
-
-//7.Создайте двумерный массив и переберите его циклами foreach, for и while
-echo '<br>';
-$ThirdArr = [1,[2,3],4];
-function sumArr($ThirdArr) {
-    $sum = 0;
-    foreach($ThirdArr as $values) {
-        if (is_array($values)) {
-            $sum += sumArr($values);
-        } else {
-            $sum += $values;
-        }
+    public function getNumBuilding()
+    {
+        return $this->numBuilding;
     }
-    return $sum;
+
+    public function setNumBuilding($numBuilding)
+    {
+        $this->numBuilding = $numBuilding;
+    }
+
+    public function getNumFloors()
+    {
+        return $this->numFloors;
+    }
+
+    public function setNumFloors($numFloors)
+    {
+        $this->numFloors = $numFloors;
+    }
 }
- echo sumArr($ThirdArr);
-for ($f = 0; $f < count($ThirdArr); ++$f) {
-    echo '<br>';
-    echo $f;
+abstract class Animal
+{
+    public $name;
+    public $say;
+    public $feed;
+
+
+    public abstract function getName();
+
+    public abstract function getSay();
+
+    public abstract function getFeed();
+
 }
-$h = 0;
-while ($h <= count($ThirdArr)){
-    $h++;
-    echo '<br>';
-    echo $h;
+
+class Cat extends Animal
+{
+    public  function getName()
+    {
+
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public  function getSay()
+    {
+
+    }
+
+    public function setSay($say)
+    {
+        $this->say = $say;
+    }
+
+    public function getFeed()
+    {
+
+    }
+
+    public function setFeed($feed)
+    {
+        $this->feed = $feed;
+    }
 }
+
+class Dog extends Animal
+{
+    public  function getName()
+    {
+
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public  function getSay()
+    {
+
+    }
+
+    public function setSay($say)
+    {
+        $this->say = $say;
+    }
+
+    public function getFeed()
+    {
+
+    }
+
+    public function setFeed($feed)
+    {
+        $this->feed = $feed;
+    }
+}
+
+class Cow extends Animal
+{
+    public  function getName()
+    {
+
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public  function getSay()
+    {
+
+    }
+
+    public function setSay($say)
+    {
+        $this->say = $say;
+    }
+
+    public function getFeed()
+    {
+
+    }
+
+    public function setFeed($feed)
+    {
+        $this->feed = $feed;
+    }
+}
+abstract class Publication {
+
+    private $workers;
+
+    protected function getWorkers()
+    {
+        return $this->workers;
+    }
+
+    protected function setWorkers($workers)
+    {
+        $this->workers = $workers;
+    }
+    abstract public function do_print();
+}
+
+class News extends Publication {
+
+    public function do_print() {
+        echo '<h4>Новость</h4>';
+    }
+}
+class Announcement extends Publication {
+    public function do_print() {
+        echo '<h4>Объявление</h4>';
+    }
+}
+class Article extends Publication {
+
+    public function do_print() {
+        echo '<h4>Статья</h4>';
+    }
+}
+
+$publications[] = new News();
+$publications[] = new Announcement();
+$publications[] = new Article();
+
+foreach ($publications as $publication) {
+    if ($publication instanceof Publication) {
+        $publication->do_print();
+    }
+    else {
+        echo 'Error';
+    }
+}
+class Calculator
+{
+    public $addition = 5+5;
+    public $subtraction;
+    public $multiplication;
+    public $division;
+    public $exponentiation;
+    public $rootExtraction;
+
+    public function getAddition()
+    {
+        return $this->addition;
+    }
+
+    public function getDivision()
+    {
+        return $this->division;
+    }
+
+    public function getExponentiation()
+    {
+        return $this->exponentiation;
+    }
+
+    public function getMultiplication()
+    {
+        return $this->multiplication;
+    }
+
+    public function getRootExtraction()
+    {
+        return $this->rootExtraction;
+    }
+
+    public function getSubtraction()
+    {
+        return $this->subtraction;
+    }
+
+    public function setAddition($addition)
+    {
+        $this->addition = $addition;
+    }
+
+    public function setDivision($division)
+    {
+        $this->division = $division;
+    }
+
+    public function setExponentiation($exponentiation)
+    {
+        $this->exponentiation = $exponentiation;
+    }
+
+    public function setMultiplication($multiplication)
+    {
+        $this->multiplication = $multiplication;
+    }
+
+    public function setRootExtraction($rootExtraction)
+    {
+        $this->rootExtraction = $rootExtraction;
+    }
+
+    public function setSubtraction($subtraction)
+    {
+        $this->subtraction = $subtraction;
+    }
+
+}
+$result = new Calculator();
+$result->setAddition(5+5);
+echo $result->getAddition() . '<br>';
+$result->setSubtraction(10-1);
+echo $result->getSubtraction() . '<br>';
+$result->setMultiplication(5*5);
+echo $result->getMultiplication() . '<br>';
+$result->setDivision(100/5);
+echo $result->getDivision() . '<br>';
+$result->setExponentiation(pow(5,2));
+echo $result->getExponentiation() . '<br>';
+$result->setRootExtraction(sqrt(9));
+echo $result->getRootExtraction();
 
